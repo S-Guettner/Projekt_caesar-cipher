5118deb603cd65f2138e84f961e32f5064bbbf55
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-06 11:05:47 +0100

    working

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
+const textInput = document.querySelector("#textInput")
+const inputKey = document.querySelector("#inputKey")
+const cypher = document.querySelector("#cypher")
+const decrypt = document.querySelector("#decrypt")
+const textOutput = document.querySelector("#textOutput")
+
+
 let letterArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-let testInput = "Helloz"
+let text = "hello"
 
 const textInputArr = []
 const textOutputArr = []
-let key = 3
-let output = []
+/* let key = 3 */
+let outputArr = []
 
+cypher.addEventListener('click', () => {
+    let key = parseInt(inputKey.value)
+    let text = textInput.value
 
-for(let i = 0; i < testInput.length; i++){
-    textInputArr.push(testInput.charAt(i).toUpperCase())
+for(let i = 0; i < text.length; i++){
+    textInputArr.push(text.charAt(i).toUpperCase())
     console.log(textInputArr)
     /* console.log(letterArray.indexOf(textInputArr[i])) */
     textOutputArr.push(letterArray.indexOf(textInputArr[i]))
@@ -21,6 +31,31 @@ textOutputArr.map((item) => {
     if(item + key > 25){
         item -= 26
     }
-    output.push(letterArray[item + key])
-    console.log(output)
+    outputArr.push(letterArray[item + key])
+    console.log(outputArr)
+    })
+    textOutput.innerHTML = outputArr.join("")
 })
+
+decrypt.addEventListener('click', () => {
+        let key = parseInt(inputKey.value)
+        let text = textInput.value
+    
+    for(let i = 0; i < text.length; i++){
+        textInputArr.push(text.charAt(i).toUpperCase())
+        console.log(textInputArr)
+        /* console.log(letterArray.indexOf(textInputArr[i])) */
+        textOutputArr.push(letterArray.indexOf(textInputArr[i]))
+        console.log(textOutputArr)
+    }
+    
+    textOutputArr.map((item) => {
+        console.log(item)
+        if(item + key > 25){
+            item -= 26
+        }
+        outputArr.push(letterArray[item - key])
+        console.log(outputArr)
+        })
+        textOutput.innerHTML = outputArr.join("")
+    })
